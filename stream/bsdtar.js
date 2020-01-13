29ca04f837b12aa47d6530e76c7d1fd9f0615c66
missinglink <insomnia@rcpt.at>
2020-01-13 13:44:38 +0100

    bsdtar: more portable syntax

--- stream/bsdtar.js
+++ stream/bsdtar.js
@@ -2,11 +2,11 @@ const shell = require('./shell')
 
 module.exports.extract = (...args) => shell.duplex(
   'bsdtar',
-  ['-xO', ...args]
+  ['-xOf', '-', ...args]
 )
 
 // man page: the file listing will be written to stderr rather than the usual stdout
 module.exports.list = (...args) => shell.duplexStderr(
   'bsdtar',
-  ['-tO', ...args]
+  ['-tOf', '-', ...args]
 )
