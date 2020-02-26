8fdc7ee763f94b01ca6e24e812703c878e123a27
missinglink <insomnia@rcpt.at>
2020-02-26 11:59:24 +0100

    fix path issue

--- stream/wof.js
+++ stream/wof.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const modulePath = path.join(__dirname, '/../')
+const modulePath = path.join(__dirname, '/..')
 const shell = require('./shell')
 
 module.exports = (...args) => {
