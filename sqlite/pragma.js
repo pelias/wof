2bcf26ab36eaa73aeeccceada07dfc178576515f
missinglink <insomnia@rcpt.at>
2020-03-04 16:22:51 +0100

    sqlite: refactor pragmas

--- sqlite/pragma.js
+++ sqlite/pragma.js
@@ -3,19 +3,35 @@ module.exports.write = (db) => {
   db.prepare('PRAGMA journal_mode=MEMORY').run()
   db.prepare('PRAGMA locking_mode=NORMAL').get()
   db.prepare('PRAGMA default_cache_size=2000').run()
-  db.prepare('PRAGMA synchronous=OFF').run()
   db.prepare('PRAGMA foreign_keys=OFF').run()
   db.prepare('PRAGMA temp_store=MEMORY').run()
   db.prepare('PRAGMA page_size=4096').run()
   db.prepare('PRAGMA cache_size=2000').run()
   db.prepare('PRAGMA recursive_triggers=OFF').run()
+
+  module.exports.synchronous(db, 'OFF')
+}
+
+// this setting is proplematic as the data may or
+// may not be written to the disk when the process exits.
+// The advantage is in increased import speed.
+// This is generally safe to use if you don't need
+// to access the database in another thread
+// immediately after creating it.
+module.exports.synchronous = (db, value) => {
+  db.prepare(`PRAGMA synchronous=${value || 'OFF'}`).run()
 }
 
 // improve query-time performance
 module.exports.read = (db) => {
   db.prepare('PRAGMA journal_mode=OFF').run()
   db.prepare('PRAGMA locking_mode=NORMAL').get()
-  db.prepare('PRAGMA synchronous=OFF').run()
-  db.prepare('PRAGMA mmap_size=268435456').get()
   db.prepare('PRAGMA recursive_triggers=OFF').run()
+
+  module.exports.synchronous(db, 'OFF')
+}
+
+// enable mmap mode (read caching in RAM via the kernel)
+module.exports.mmap = (db) => {
+  db.prepare('PRAGMA mmap_size=268435456').get()
 }
--- sqlite/pragma.test.js
+++ sqlite/pragma.test.js
@@ -26,17 +26,18 @@ module.exports.write = (test) => {
     t.equals(db.pragmas[1].action, 'get')
     t.equals(db.pragmas[2].sql, 'PRAGMA default_cache_size=2000')
     t.equals(db.pragmas[2].action, 'run')
-    t.equals(db.pragmas[3].sql, 'PRAGMA synchronous=OFF')
+    t.equals(db.pragmas[3].sql, 'PRAGMA foreign_keys=OFF')
     t.equals(db.pragmas[3].action, 'run')
-    t.equals(db.pragmas[4].sql, 'PRAGMA foreign_keys=OFF')
+    t.equals(db.pragmas[4].sql, 'PRAGMA temp_store=MEMORY')
     t.equals(db.pragmas[4].action, 'run')
-    t.equals(db.pragmas[5].sql, 'PRAGMA temp_store=MEMORY')
+    t.equals(db.pragmas[5].sql, 'PRAGMA page_size=4096')
     t.equals(db.pragmas[5].action, 'run')
-    t.equals(db.pragmas[6].sql, 'PRAGMA page_size=4096')
+    t.equals(db.pragmas[6].sql, 'PRAGMA cache_size=2000')
     t.equals(db.pragmas[6].action, 'run')
-    t.equals(db.pragmas[7].sql, 'PRAGMA cache_size=2000')
+    t.equals(db.pragmas[7].sql, 'PRAGMA recursive_triggers=OFF')
     t.equals(db.pragmas[7].action, 'run')
-    t.equals(db.pragmas[8].sql, 'PRAGMA recursive_triggers=OFF')
+
+    t.equals(db.pragmas[8].sql, 'PRAGMA synchronous=OFF')
     t.equals(db.pragmas[8].action, 'run')
 
     t.end()
@@ -48,17 +49,52 @@ module.exports.read = (test) => {
     const db = new MockDatabase()
     pragma.read(db)
 
-    t.equals(db.pragmas.length, 5)
+    t.equals(db.pragmas.length, 4)
     t.equals(db.pragmas[0].sql, 'PRAGMA journal_mode=OFF')
     t.equals(db.pragmas[0].action, 'run')
     t.equals(db.pragmas[1].sql, 'PRAGMA locking_mode=NORMAL')
     t.equals(db.pragmas[1].action, 'get')
-    t.equals(db.pragmas[2].sql, 'PRAGMA synchronous=OFF')
+    t.equals(db.pragmas[2].sql, 'PRAGMA recursive_triggers=OFF')
     t.equals(db.pragmas[2].action, 'run')
-    t.equals(db.pragmas[3].sql, 'PRAGMA mmap_size=268435456')
-    t.equals(db.pragmas[3].action, 'get')
-    t.equals(db.pragmas[4].sql, 'PRAGMA recursive_triggers=OFF')
-    t.equals(db.pragmas[4].action, 'run')
+
+    t.equals(db.pragmas[3].sql, 'PRAGMA synchronous=OFF')
+    t.equals(db.pragmas[3].action, 'run')
+
+    t.end()
+  })
+}
+
+module.exports.synchronous = (test) => {
+  test('synchronous - default', (t) => {
+    const db = new MockDatabase()
+    pragma.synchronous(db)
+
+    t.equals(db.pragmas.length, 1)
+    t.equals(db.pragmas[0].sql, 'PRAGMA synchronous=OFF')
+    t.equals(db.pragmas[0].action, 'run')
+
+    t.end()
+  })
+  test('synchronous - custom', (t) => {
+    const db = new MockDatabase()
+    pragma.synchronous(db, 'NORMAL')
+
+    t.equals(db.pragmas.length, 1)
+    t.equals(db.pragmas[0].sql, 'PRAGMA synchronous=NORMAL')
+    t.equals(db.pragmas[0].action, 'run')
+
+    t.end()
+  })
+}
+
+module.exports.mmap = (test) => {
+  test('mmap', (t) => {
+    const db = new MockDatabase()
+    pragma.mmap(db)
+
+    t.equals(db.pragmas.length, 1)
+    t.equals(db.pragmas[0].sql, 'PRAGMA mmap_size=268435456')
+    t.equals(db.pragmas[0].action, 'get')
 
     t.end()
   })
