1709748444a11a433a712352d46b4c3930b88477
missinglink <insomnia@rcpt.at>
2020-03-04 10:56:49 +0100

    spawn_duplex: correct implementation of a duplex stream for a child process

--- stream/bsdtar.js
+++ stream/bsdtar.js
@@ -6,7 +6,8 @@ module.exports.extract = (...args) => shell.duplex(
 )
 
 // man page: the file listing will be written to stderr rather than the usual stdout
-module.exports.list = (...args) => shell.duplexStderr(
-  'bsdtar',
-  ['-tOf', '-', ...args]
+// the bash-foo is used in order to 'switch' stderr and stderr streams
+module.exports.list = (...args) => shell.duplex(
+  'bash',
+  ['-c', `set -o noglob; 3>&1 1>&2 2>&3 bsdtar -tOf - ` + args.join(' ')]
 )
