ab39877c84f874b29d08f881fed9956a04a8e94f
missinglink <insomnia@rcpt.at>
2020-08-22 19:44:26 +0200

    fix(centroid): improved centroid calc for SPR

--- whosonfirst/geometry.test.js
+++ whosonfirst/geometry.test.js
@@ -58,3 +58,70 @@ module.exports.bbox = (test) => {
     t.end()
   })
 }
+
+module.exports.centroid = (test) => {
+  test('centroid - default', (t) => {
+    t.deepEquals(geometry.centroid({}), { lon: 0, lat: 0 })
+    t.end()
+  })
+  test('centroid - both namespace properties must be set', (t) => {
+    t.deepEquals(geometry.centroid({
+      properties: {
+        'geom:longitude': 1.1
+      }
+    }), { lon: 0, lat: 0 })
+    t.end()
+  })
+  test('centroid - both namespace properties must be valid', (t) => {
+    t.deepEquals(geometry.centroid({
+      properties: {
+        'geom:longitude': 1.1,
+        'geom:latitude': 'a'
+      }
+    }), { lon: 0, lat: 0 })
+    t.end()
+  })
+  test('centroid - namespace properties can be strings', (t) => {
+    t.deepEquals(geometry.centroid({
+      properties: {
+        'geom:longitude': '1.1',
+        'geom:latitude': '2.2'
+      }
+    }), { lon: 1.1, lat: 2.2 })
+    t.end()
+  })
+
+  test('centroid - use "geom" namespace where available', (t) => {
+    t.deepEquals(geometry.centroid({
+      properties: {
+        'geom:longitude': 1.1,
+        'geom:latitude': 2.2
+      }
+    }), { lon: 1.1, lat: 2.2 })
+    t.end()
+  })
+  test('centroid - prefer "reversegeo" namespace', (t) => {
+    t.deepEquals(geometry.centroid({
+      properties: {
+        'geom:longitude': 1.1,
+        'geom:latitude': 2.2,
+        'reversegeo:longitude': 3.3,
+        'reversegeo:latitude': 4.4
+      }
+    }), { lon: 3.3, lat: 4.4 })
+    t.end()
+  })
+  test('centroid - prefer "lbl" namespace', (t) => {
+    t.deepEquals(geometry.centroid({
+      properties: {
+        'geom:longitude': 1.1,
+        'geom:latitude': 2.2,
+        'reversegeo:longitude': 3.3,
+        'reversegeo:latitude': 4.4,
+        'lbl:longitude': 5.5,
+        'lbl:latitude': 6.6
+      }
+    }), { lon: 5.5, lat: 6.6 })
+    t.end()
+  })
+}
