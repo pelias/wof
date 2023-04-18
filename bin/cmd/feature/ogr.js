ecd4705eba19fb96975f4dd33df45f477c2cadf3
missinglink <insomnia@rcpt.at>
2023-04-18 12:26:43 +0200

    feat(bin/feature/ogr): add --rm flag

--- bin/cmd/feature/ogr.js
+++ bin/cmd/feature/ogr.js
@@ -18,6 +18,12 @@ module.exports = {
       required: true,
       describe: 'ogr2ogr `-f` argument. see: https://gdal.org/programs/ogr2ogr.html#cmdoption-ogr2ogr-f'
     })
+    yargs.option('unlink', {
+      type: 'boolean',
+      default: false,
+      alias: 'rm',
+      describe: 'ogr2ogr `-overwrite` argument. see: https://gdal.org/programs/ogr2ogr.html#cmdoption-ogr2ogr-overwrite'
+    })
   },
   handler: (argv) => {
     const tap = new Stream.PassThrough()
@@ -25,6 +31,7 @@ module.exports = {
       tap.pipe(stream.shell.duplex('ogr2ogr', [
         '-f', argv.format,
         '-a_srs', 'EPSG:4326',
+        ...(argv.unlink ? ['-overwrite'] : []),
         ...formatSpecificArgs(argv.format.toLowerCase()),
         argv.dst, '/vsistdin?buffer_limit=-1/'
       ]))
